e18ed69f52d5f88a2acf0c8c44fea2819ecb0b78
unnatiselfhelpgroup-creator <unnatiselfhelpgroup@gmail.com>
2026-06-14 22:11:57 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,13 +1,1 @@
-function login() {
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("password").value.trim();
 
-  auth.signInWithEmailAndPassword(email, password)
-    .then(() => {
-      alert("✅ Login Successful");
-      window.location.href = "admin-dashboard.html";
-    })
-    .catch((error) => {
-      alert("❌ " + error.message);
-    });
-}
