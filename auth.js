5bfcc84960341e81abbf568971d9c38392597f0e
unnatiselfhelpgroup-creator <unnatiselfhelpgroup@gmail.com>
2026-06-14 19:41:13 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,13 +1,1 @@
-function login() {
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("password").value.trim();
 
-  if (
-    email === "unnatiselfhelpgroup@gmail.com" &&
-    password === "Vinod@1981"
-  ) {
-    window.location.href = "admin-dashboard.html";
-  } else {
-    alert("❌ गलत Email या Password");
-  }
-}
