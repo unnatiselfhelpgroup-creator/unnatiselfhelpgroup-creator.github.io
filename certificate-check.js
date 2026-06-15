9b2ca73c5bd2b651c8c0bbce3680db543ae1e2a9
unnatiselfhelpgroup-creator <unnatiselfhelpgroup@gmail.com>
2026-06-15 06:25:21 +0530

    Update certificate-check.js

--- certificate-check.js
+++ certificate-check.js
@@ -1,1 +1,144 @@
+import {
+initializeApp
+}
+from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 
+import {
+getFirestore,
+doc,
+getDoc
+}
+from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey:
+"AIzaSyCNEZ03VUTOD-eJjPsMP4b0Ykh4eiigqPQ",
+
+authDomain:
+"ngo-certificate-system.firebaseapp.com",
+
+projectId:
+"ngo-certificate-system",
+
+storageBucket:
+"ngo-certificate-system.firebasestorage.app",
+
+messagingSenderId:
+"664351169113",
+
+appId:
+"1:664351169113:web:3c476e199d369615c6ef48"
+};
+
+const app =
+initializeApp(firebaseConfig);
+
+const db =
+getFirestore(app);
+
+window.checkCert =
+async function () {
+
+const certNo =
+document.getElementById("certInput")
+.value.trim();
+
+const result =
+document.getElementById("resultBox");
+
+if (!certNo) {
+alert(
+"कृपया प्रमाणपत्र संख्या दर्ज करें।"
+);
+return;
+}
+
+try {
+
+const ref =
+doc(
+db,
+"Certificates",
+certNo
+);
+
+const snap =
+await getDoc(ref);
+
+result.style.display =
+"block";
+
+if (snap.exists()) {
+
+const d =
+snap.data();
+
+result.className =
+"result valid";
+
+result.innerHTML = `
+<h3>✅ प्रमाणपत्र मान्य है</h3>
+
+<p><b>प्रमाणपत्र संख्या :</b>
+${certNo}</p>
+
+<p><b>नाम :</b>
+${d.name || ""}</p>
+
+<p><b>पिता/पति का नाम :</b>
+${d.father_name || ""}</p>
+
+<p><b>वॉलंटियर आईडी :</b>
+${d.volunteer_id || ""}</p>
+
+<p><b>सेवा अवधि :</b>
+${d.duration || ""}</p>
+
+<p><b>सामाजिक कार्य :</b>
+${d.work_details || ""}</p>
+
+<p><b>जारी तिथि :</b>
+${d.issueDate || ""}</p>
+
+<p><b>स्थिति :</b>
+${d.status || "Verified"}</p>
+
+<p><b>NITI Aayog Unique ID :</b>
+US-2016/0108273</p>
+`;
+
+}
+else {
+
+result.className =
+"result invalid";
+
+result.innerHTML = `
+<h3>❌ प्रमाणपत्र संख्या उपलब्ध नहीं है</h3>
+
+<p>
+कृपया सही Certificate Number दर्ज करें।
+</p>
+`;
+
+}
+
+}
+catch (error) {
+
+console.log(error);
+
+result.style.display =
+"block";
+
+result.className =
+"result invalid";
+
+result.innerHTML = `
+<h3>❌ Error</h3>
+<p>${error.message}</p>
+`;
+
+}
+
+};
