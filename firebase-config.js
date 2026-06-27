8466f14cfc910f9b004fb1211fb39d4d656f4e16
unnatiselfhelpgroup-creator <unnatiselfhelpgroup@gmail.com>
2026-06-27 17:09:09 +0530

    Remove Firebase config and initialization
    Removed Firebase configuration and initialization code.

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-storage.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCNEZ03VUTOD-eJjPsMP4b0Ykh4eiigqPQ",
-    authDomain: "ngo-certificate-system.firebaseapp.com",
-    projectId: "ngo-certificate-system",
-    storageBucket: "ngo-certificate-system.firebasestorage.app",
-    messagingSenderId: "664351169113",
-    appId: "1:664351169113:web:3c476e199d369615c6ef48"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
